ffb915037ab528b220f4d7136b356bfd9e96a240
Uriel Ortega <urielortega2509@gmail.com>
2023-05-24 10:15:48 -0600

    Initial Commit

---
+++ HabitTrack/HabitTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackApp.swift
+//  HabitTrack
+//
+//  Created by Uriel Ortega on 24/05/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
